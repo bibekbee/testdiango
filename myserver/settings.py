179fbf5616429d17c925b16edc6cf9e8e7befbaa
Bibek Bhattarai <37708553+bibekbee@users.noreply.github.com>
2024-11-30 22:33:29 +0545

    Update settings.py
    update allowedhost

--- myserver/settings.py
+++ myserver/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@2xl+y+sx3(zhn#oagl#!biwk26b_0fc0$e7l7mi=vc9&rzup(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['testdiango.vercel.app']
 
 
 # Application definition
